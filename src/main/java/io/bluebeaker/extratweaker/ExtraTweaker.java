f190cc2f21a5a0bbf66249c9bad249defa89a8c1
blue-beaker <yyy5566778899@126.com>
2024-09-29 21:57:26 +0800

    init

--- src/main/java/io/bluebeaker/extratweaker/ExtraTweaker.java
+++ src/main/java/io/bluebeaker/extratweaker/ExtraTweaker.java
@@ -1,7 +1,12 @@
-package io.bluebeaker.examplemod;
+package io.bluebeaker.extratweaker;
+
+import java.util.LinkedList;
+import java.util.List;
 
 import org.apache.logging.log4j.Logger;
 
+import crafttweaker.CraftTweakerAPI;
+import crafttweaker.IAction;
 import net.minecraft.server.MinecraftServer;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.common.config.ConfigManager;
@@ -9,22 +14,25 @@ import net.minecraftforge.common.config.Config.Type;
 import net.minecraftforge.fml.client.event.ConfigChangedEvent.OnConfigChangedEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLLoadCompleteEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
-@Mod(modid = ExampleMod.MODID, name = ExampleMod.NAME, version = ExampleMod.VERSION)
-public class ExampleMod
+@Mod(modid = ExtraTweaker.MODID, name = ExtraTweaker.NAME, version = ExtraTweaker.VERSION)
+public class ExtraTweaker
 {
-    public static final String MODID = "examplemod";
-    public static final String NAME = "ExampleMod";
+    public static final String MODID = "extratweaker";
+    public static final String NAME = "ExtraTweaker";
     public static final String VERSION = "1.0";
     
+    public static final List<IAction> LATE_REMOVALS = new LinkedList<>();
+    public static final List<IAction> LATE_ADDITIONS = new LinkedList<>();
     public MinecraftServer server;
 
     private static Logger logger;
     
-    public ExampleMod() {
+    public ExtraTweaker() {
         MinecraftForge.EVENT_BUS.register(this);
     }
     
@@ -42,6 +50,19 @@ public class ExampleMod
         if (event.getModID().equals(MODID)) {
             ConfigManager.sync(MODID, Type.INSTANCE);
         }
+    }
+
+    @Mod.EventHandler
+    public void loadComplete(FMLLoadCompleteEvent event) {
+        try {
+            LATE_REMOVALS.forEach(CraftTweakerAPI::apply);
+            LATE_ADDITIONS.forEach(CraftTweakerAPI::apply);
+        } catch(Exception e) {
+            e.printStackTrace();
+            CraftTweakerAPI.logError("Error while applying actions", e);
+        }
+        LATE_REMOVALS.clear();
+        LATE_ADDITIONS.clear();
     }
 
     public void logInfo(String log){
---
+++ src/main/java/io/bluebeaker/extratweaker/thermal/Extruder.java
@@ -0,0 +1,57 @@
+package io.bluebeaker.extratweaker.thermal;
+
+import cofh.thermalexpansion.util.managers.machine.ExtruderManager;
+import crafttweaker.IAction;
+import crafttweaker.annotations.ModOnly;
+import crafttweaker.annotations.ZenRegister;
+import crafttweaker.api.item.IItemStack;
+import crafttweaker.api.minecraft.CraftTweakerMC;
+import io.bluebeaker.extratweaker.ExtraTweaker;
+import net.minecraftforge.fluids.FluidRegistry;
+import net.minecraftforge.fluids.FluidStack;
+import stanhebben.zenscript.annotations.ZenClass;
+import stanhebben.zenscript.annotations.ZenMethod;
+
+@ZenRegister
+@ZenClass("mods.thermalexpansion.Extruder")
+@ModOnly("thermalexpansion")
+public class Extruder {
+    @ZenMethod
+    public void addRecipeIgneous(IItemStack output, int lava, int water, int energy) {
+        ExtraTweaker.LATE_ADDITIONS.add(energy, new ExtruderRecipe(output, lava, water, energy, false));
+    }
+
+    @ZenMethod
+    public void addRecipeSedimentary(IItemStack output, int lava, int water, int energy) {
+        ExtraTweaker.LATE_ADDITIONS.add(energy, new ExtruderRecipe(output, lava, water, energy, true));
+    }
+
+    public class ExtruderRecipe implements IAction{
+        IItemStack output;
+        int lava;
+        int water;
+        int energy;
+        boolean isSedimentary;
+        public ExtruderRecipe(IItemStack output, int lava, int water, int energy, boolean isSedimentary){
+            this.output=output;
+            this.lava=lava;
+            this.water=water;
+            this.energy=energy;
+            this.isSedimentary=isSedimentary;
+        }
+        @Override
+        public void apply() {
+            if(!this.isSedimentary){
+                ExtruderManager.addRecipeIgneous(energy, CraftTweakerMC.getItemStack(output),
+                new FluidStack(FluidRegistry.LAVA, lava), new FluidStack(FluidRegistry.WATER, water));
+            }else{
+                ExtruderManager.addRecipeSedimentary(energy, CraftTweakerMC.getItemStack(output),
+                new FluidStack(FluidRegistry.LAVA, lava), new FluidStack(FluidRegistry.WATER, water));
+            }
+        }
+        @Override
+        public String describe() {
+            return "Adding "+(this.isSedimentary?"Sedimentary":"Igneous")+"Extruder recipe: Lava*"+lava+"+ Water*"+water+"+"+energy+"RF = "+output.getDisplayName();
+        }
+    }
+}
